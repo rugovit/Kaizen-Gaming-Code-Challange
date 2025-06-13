4f89f85feba43cb173f05ed7f0d43183b0a5a432
Darko Martinovic <darko.martinovicc@gmail.com>
2025-06-13 11:49:56 +0200

    bug fix on DatabaseModule.kt

--- app/src/main/java/com/rugovit/kaizengamingcodechallange/di/data/DatabaseModule.kt
+++ app/src/main/java/com/rugovit/kaizengamingcodechallange/di/data/DatabaseModule.kt
@@ -1,6 +1,7 @@
 package com.rugovit.kaizengamingcodechallange.di.data
 
 import androidx.room.Room
+import com.rugovit.kaizengamingcodechallange.core.common.TransactionRunner
 import com.rugovit.kaizengamingcodechallange.data.database.AppDatabase
 import com.rugovit.kaizengamingcodechallange.data.database.RoomTransactionRunner
 import org.koin.android.ext.koin.androidApplication
@@ -18,7 +19,7 @@ val databaseModule = module {
     // Provide SportDao as a singleton, retrieved from AppDatabase
     single { get<AppDatabase>().sportDao() }
     // add transaction runner
-    single {
+    single<TransactionRunner>{
          RoomTransactionRunner(get<AppDatabase>())
     }
 }
